28900f5ab59d6b513be0811f36cee7a40015383e
devHTak <qkrgusxkr1@gmail.com>
2022-05-08 14:18:32 +0900

    circuit breaker 적용

--- chap07_cricuit_breaker/user-service/src/main/java/com/example/userservice/inbound/service/UserService.java
+++ chap07_cricuit_breaker/user-service/src/main/java/com/example/userservice/inbound/service/UserService.java
@@ -3,12 +3,17 @@ package com.example.userservice.inbound.service;
 import com.example.userservice.inbound.entity.Users;
 import com.example.userservice.inbound.repository.UserRepository;
 import com.example.userservice.outbound.adaptor.client.BoardClient;
+import com.example.userservice.outbound.dto.PostResponse;
 import com.example.userservice.outbound.dto.UserResource;
 import com.example.userservice.outbound.dto.UserResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,16 +22,24 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final BoardClient boardClient;
+    private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
 
     public UserResponse getUserByUserId(String userId) {
         Users user = userRepository.findByUserId(userId)
                 .orElseThrow(IllegalArgumentException::new);
 
+        // circuitbreaker 사용
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("board-service");
+        List<PostResponse> posts = circuitBreaker.run(
+                () -> boardClient.getPostsByUserId(userId).getBody(),
+                (throwable) -> new ArrayList<>()
+        );
+
         return UserResponse.builder()
                     .userId(user.getUserId())
                     .userName(user.getUserName())
                     .createdAt(user.getCreatedAt())
-                    .posts(boardClient.getPostsByUserId(userId).getBody())
+                    .posts(posts)
                     .build();
     }
 
